c503a8d000b52a631fe9c850968d7cc208920d33
GhostWheat <32331476+GhostWheat@users.noreply.github.com>
2023-04-16 21:27:29 -0500

    still fixing insertions

--- car_dealership_database_UPDATING.sql
+++ car_dealership_database_UPDATING.sql
@@ -88,7 +88,7 @@
 -- LANGUAGE plpgsql
 -- AS $MAIN$
 -- BEGIN
--- 	INSERT INTO tickets
+-- 	INSERT INTO service_tickets
 -- 	VALUES(DEFAULT,car_serial,mech_id,parts_needed,date,price);
 -- END;
 -- $MAIN$;
@@ -133,50 +133,70 @@
 -- $MAIN$;
 
 -------------------------------------------
+
+-- OK, small problem: the SERIAL value/variable is apparently shared across all the tables somehow??
+-- so after inserting the 4 sellers, the very first customer inserted has a serial of 5...
+-- SO I am manually resetting before each new table.
+
 -- FIRST: Sellers, Customers, Cards On File
-SELECT insert_sellers("John","Wick");
-SELECT insert_sellers("Theodore","Logan");
-SELECT insert_sellers("William","Preston");
-SELECT insert_sellers("Bob","Robertson");
 
-SELECT insert_customers("Swan","E-G");
-SELECT insert_customers("Abe","Lincoln");
-SELECT insert_customers("Nappy","Bonaparte");
-SELECT insert_customers("Sigmund","Freud");
+ALTER SEQUENCE sellers_seller_id_seq RESTART;
+SELECT insert_sellers('John','Wick');
+SELECT insert_sellers('Theodore','Logan');
+SELECT insert_sellers('William','Preston');
+SELECT insert_sellers('Bob','Robertson');
 
-SELECT cards("Swan E-G","1111222233334444","1234 Main Street Chicago","60666","01/25",666,1);
-SELECT cards("Abraham Lincoln","5555666677778888","4321 Main Street Springfield","60000","01/24",111,2);
-SELECT cards("Napoleon Jiberious Bonaparte","1234432112349999","101 Rue Ste Germaine Paris","4444-0123","01/25",989,3);
-SELECT cards("Sigmund H Freud","666444333111111111","123 Ztreyte Wien","99998","09/26",010,4);
+
+ALTER SEQUENCE customers_customer_id_seq RESTART;
+SELECT insert_customers('Swan','E-G','123456789');
+SELECT insert_customers('Abe','Lincoln','111111111');
+SELECT insert_customers('Nappy','Bonaparte','123123123');
+SELECT insert_customers('Sigmund','Freud','969696969');
+
+-- DELETE FROM customers
+SELECT * FROM cards_on_file
+ALTER SEQUENCE cards_on_file_card_id_seq RESTART;
+SELECT insert_cards('Swan E-G','1111222233334444','1234 Main Street Chicago','60666','01/25',666,1);
+SELECT insert_cards('Abraham Lincoln','5555666677778888','4321 Main Street Springfield','60000','01/24',111,2);
+SELECT insert_cards('Napoleon Jiberious Bonaparte','1234432112349999','101 Rue Ste Germaine Paris','4444-0123','01/25',989,3);
+SELECT insert_cards('Sigmund H Freud','666444333111111111','123 Ztreyte Wien','99998','09/26',010,4);
 
 -- SECOND: Cars Owned, Sales Invoices, Mechanics
-SELECT insert_cars_owned("Toyota","Acura",2005,"Red",1,"454545454545ABXY");
-SELECT insert_cars_owned("Toyota","Acura",2023,"Banana Yellow",1,"23232323232323LALO");
-SELECT insert_cars_owned("French","Horse",1810,"Chestnut",3,"NEEEEEEEEEEIIIGH");
-SELECT insert_cars_owned("Bugatti","Chiron Sport",2023,"Fire-Engine Red",4,"P3NF1FT33NCLUB");
+ALTER SEQUENCE cars_owned_car_serial_seq RESTART;
+SELECT insert_cars_owned('Toyota','Acura',2005,'Red',1,'454545454545ABXY');
+SELECT insert_cars_owned('Toyota','Acura',2023,'Banana Yellow',1,'23232323232323LALO');
+SELECT insert_cars_owned('French','Horse',1810,'Chestnut',3,'NEEEEEEEEEEIIIGH');
+SELECT insert_cars_owned('Bugatti','Chiron Sport',2023,'Fire-Engine Red',4,'P3NF1FT33NCLUB');
 
-SELECT insert_sales_invoices(1,1,05/26/2005,20000.99);
-SELECT insert_sales_invoices(1,2,3/17/2023,20001.08);
-SELECT insert_sales_invoices(2,3,6/17/1815,9999999999.99);
-SELECT insert_sales_invoices(3,4,1/01/2023,20002.08);
+ALTER SEQUENCE sales_invoices_invoice_id_seq RESTART;
+SELECT insert_sales_invoices(1,1,'05/26/2005',20000.99);
+SELECT insert_sales_invoices(1,2,'3/17/2023',20001.08);
+SELECT insert_sales_invoices(2,3,'6/17/1815',9999999999.99);
+SELECT insert_sales_invoices(3,4,'1/01/2023',20002.08);
 
-SELECT insert_mechanics("Big","Joe");
-SELECT insert_mechanics("Large","Lu");
-SELECT insert_mechanics("Huge","Henri");
-SELECT insert_mechanics("Lil","Guy");
+ALTER SEQUENCE mechanics_mechanic_id_seq RESTART;
+SELECT insert_mechanics('Big','Joe');
+SELECT insert_mechanics('Large','Lu');
+SELECT insert_mechanics('Huge','Henri');
+SELECT insert_mechanics('Lil','Guy');
+
+select * from mechanics
 
 -- THIRD: Service Tickets, Parts Catalogue, Parts Requisitions
-SELECT insert_tickets(1,1,true,01/02/2022,23.12);
-SELECT insert_tickets(1,2,true,01/02/2022,23.12);
-SELECT insert_tickets(1,3,true,01/02/2022,23.12;
-SELECT insert_tickets(2,4,false,01/02/2022.77.11);
+ALTER SEQUENCE service_tickets_serv_ticket_id_seq RESTART;
+SELECT insert_tickets(1,1,true,'1/2/2022',23.12);
+SELECT insert_tickets(1,2,true,'1/2/2022',23.12);
+SELECT insert_tickets(1,3,true,'1/2/2022',23.12);
+SELECT insert_tickets(2,4,False,'1/2/2022',77.11);
 
-SELECT insert_parts_catalogue("MEGABASS SUBWOOFER",1.00);
-SELECT insert_parts_catalogue("PLASMA RIFLE, USED",1.50);
-SELECT insert_parts_catalogue("SNOW TIRES",9999.99);
-SELECT insert_parts_catalogue("");
+ALTER SEQUENCE parts_catalogue_part_id_seq RESTART;
+SELECT insert_parts_catalogue('MEGABASS SUBWOOFER',1.00);
+SELECT insert_parts_catalogue('PLASMA RIFLE, USED',1.50);
+SELECT insert_parts_catalogue('SNOW TIRES',9999.99);
+SELECT insert_parts_catalogue('SUSPENSION',2.50);
 
-SELECT insert_parts_req();
-SELECT insert_parts_req();
-SELECT insert_parts_req();
-SELECT insert_parts_req();
+ALTER SEQUENCE parts_requisitions_parts_requisition_id_seq RESTART;
+SELECT insert_parts_req(1,1);
+SELECT insert_parts_req(1,2);
+SELECT insert_parts_req(1,2);
+SELECT insert_parts_req(1,4);
